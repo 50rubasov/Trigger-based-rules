c6fb7f1088adddcc5de8764f44c99c33f86d528b
50rubasov <bigismol666@gmail.com>
2020-05-20 18:34:23 +0700

    Реализованно визуальное добавление дополнительных условий на форму(без привязки к запросу)

--- FacebookApi/FacebookApiView/CreateRuleForm.Designer.cs
+++ FacebookApi/FacebookApiView/CreateRuleForm.Designer.cs
@@ -28,45 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ConditionGroupBox = new System.Windows.Forms.GroupBox();
             this.AddCondition = new System.Windows.Forms.Button();
             this.ConditionValueTextBox = new System.Windows.Forms.NumericUpDown();
             this.ConditionOperatorComboBox = new System.Windows.Forms.ComboBox();
             this.ConditionFieldComboBox = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ActionOffRadioButton = new System.Windows.Forms.RadioButton();
+            this.ActionOnRadioButton = new System.Windows.Forms.RadioButton();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.NameTextBox = new System.Windows.Forms.TextBox();
             this.CreateButton = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.EntityTypeComboBox = new System.Windows.Forms.ComboBox();
-            this.ActionOnRadioButton = new System.Windows.Forms.RadioButton();
-            this.ActionOffRadioButton = new System.Windows.Forms.RadioButton();
-            this.ConditionGroupBox.SuspendLayout();
+            this.TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ConditionGroupBox = new System.Windows.Forms.GroupBox();
+            this.DeleteCondition = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ConditionValueTextBox)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            this.TableLayoutPanel.SuspendLayout();
+            this.ConditionGroupBox.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // ConditionGroupBox
-            // 
-            this.ConditionGroupBox.Controls.Add(this.AddCondition);
-            this.ConditionGroupBox.Controls.Add(this.ConditionValueTextBox);
-            this.ConditionGroupBox.Controls.Add(this.ConditionOperatorComboBox);
-            this.ConditionGroupBox.Controls.Add(this.ConditionFieldComboBox);
-            this.ConditionGroupBox.Location = new System.Drawing.Point(12, 196);
-            this.ConditionGroupBox.Name = "ConditionGroupBox";
-            this.ConditionGroupBox.Size = new System.Drawing.Size(362, 81);
-            this.ConditionGroupBox.TabIndex = 7;
-            this.ConditionGroupBox.TabStop = false;
-            this.ConditionGroupBox.Text = "Условия";
             // 
             // AddCondition
             // 
-            this.AddCondition.Location = new System.Drawing.Point(4, 51);
+            this.AddCondition.Location = new System.Drawing.Point(7, 21);
             this.AddCondition.Name = "AddCondition";
-            this.AddCondition.Size = new System.Drawing.Size(75, 23);
+            this.AddCondition.Size = new System.Drawing.Size(92, 22);
             this.AddCondition.TabIndex = 13;
             this.AddCondition.Text = "+";
             this.AddCondition.UseVisualStyleBackColor = true;
@@ -74,7 +65,7 @@
             // 
             // ConditionValueTextBox
             // 
-            this.ConditionValueTextBox.Location = new System.Drawing.Point(280, 21);
+            this.ConditionValueTextBox.Location = new System.Drawing.Point(279, 3);
             this.ConditionValueTextBox.Name = "ConditionValueTextBox";
             this.ConditionValueTextBox.Size = new System.Drawing.Size(75, 22);
             this.ConditionValueTextBox.TabIndex = 4;
@@ -86,9 +77,9 @@
             "Больше, чем",
             "Меньше, чем",
             "Равно"});
-            this.ConditionOperatorComboBox.Location = new System.Drawing.Point(162, 21);
+            this.ConditionOperatorComboBox.Location = new System.Drawing.Point(167, 3);
             this.ConditionOperatorComboBox.Name = "ConditionOperatorComboBox";
-            this.ConditionOperatorComboBox.Size = new System.Drawing.Size(112, 24);
+            this.ConditionOperatorComboBox.Size = new System.Drawing.Size(106, 24);
             this.ConditionOperatorComboBox.TabIndex = 3;
             this.ConditionOperatorComboBox.Text = "Больше, чем";
             this.ConditionOperatorComboBox.SelectedIndexChanged += new System.EventHandler(this.ConditionOperatorComboBox_SelectedIndexChanged);
@@ -100,9 +91,9 @@
             "Цена за результат",
             "Результаты",
             "Расходы"});
-            this.ConditionFieldComboBox.Location = new System.Drawing.Point(6, 21);
+            this.ConditionFieldComboBox.Location = new System.Drawing.Point(3, 3);
             this.ConditionFieldComboBox.Name = "ConditionFieldComboBox";
-            this.ConditionFieldComboBox.Size = new System.Drawing.Size(150, 24);
+            this.ConditionFieldComboBox.Size = new System.Drawing.Size(158, 24);
             this.ConditionFieldComboBox.TabIndex = 2;
             this.ConditionFieldComboBox.Text = "Цена за результат";
             this.ConditionFieldComboBox.SelectedIndexChanged += new System.EventHandler(this.ConditionFieldComboBox_SelectedIndexChanged);
@@ -117,6 +108,30 @@
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Действие";
+            // 
+            // ActionOffRadioButton
+            // 
+            this.ActionOffRadioButton.AutoSize = true;
+            this.ActionOffRadioButton.Checked = true;
+            this.ActionOffRadioButton.Location = new System.Drawing.Point(7, 21);
+            this.ActionOffRadioButton.Name = "ActionOffRadioButton";
+            this.ActionOffRadioButton.Size = new System.Drawing.Size(103, 21);
+            this.ActionOffRadioButton.TabIndex = 1;
+            this.ActionOffRadioButton.TabStop = true;
+            this.ActionOffRadioButton.Text = "Выключить";
+            this.ActionOffRadioButton.UseVisualStyleBackColor = true;
+            this.ActionOffRadioButton.CheckedChanged += new System.EventHandler(this.ActionOffRadioButton_CheckedChanged);
+            // 
+            // ActionOnRadioButton
+            // 
+            this.ActionOnRadioButton.AutoSize = true;
+            this.ActionOnRadioButton.Location = new System.Drawing.Point(116, 21);
+            this.ActionOnRadioButton.Name = "ActionOnRadioButton";
+            this.ActionOnRadioButton.Size = new System.Drawing.Size(93, 21);
+            this.ActionOnRadioButton.TabIndex = 0;
+            this.ActionOnRadioButton.Text = "Включить";
+            this.ActionOnRadioButton.UseVisualStyleBackColor = true;
+            this.ActionOnRadioButton.CheckedChanged += new System.EventHandler(this.ActionOnRadioButton_CheckedChanged);
             // 
             // groupBox4
             // 
@@ -137,21 +152,23 @@
             // 
             // CreateButton
             // 
-            this.CreateButton.Location = new System.Drawing.Point(16, 451);
+            this.CreateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CreateButton.Location = new System.Drawing.Point(12, 315);
             this.CreateButton.Name = "CreateButton";
-            this.CreateButton.Size = new System.Drawing.Size(99, 28);
+            this.CreateButton.Size = new System.Drawing.Size(176, 28);
             this.CreateButton.TabIndex = 10;
-            this.CreateButton.Text = "Добавить";
+            this.CreateButton.Text = "Создать правило";
             this.CreateButton.UseVisualStyleBackColor = true;
             this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
             // 
             // CloseButton
             // 
-            this.CloseButton.Location = new System.Drawing.Point(281, 451);
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.Location = new System.Drawing.Point(201, 314);
             this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(91, 29);
+            this.CloseButton.Size = new System.Drawing.Size(168, 29);
             this.CloseButton.TabIndex = 11;
-            this.CloseButton.Text = "Отменить";
+            this.CloseButton.Text = "Закрыть";
             this.CloseButton.UseVisualStyleBackColor = true;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
@@ -179,36 +196,61 @@
             this.EntityTypeComboBox.Text = "Группа объявлений";
             this.EntityTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.EntityTypeComboBox_SelectedIndexChanged);
             // 
-            // ActionOnRadioButton
+            // TableLayoutPanel
             // 
-            this.ActionOnRadioButton.AutoSize = true;
-            this.ActionOnRadioButton.Location = new System.Drawing.Point(116, 21);
-            this.ActionOnRadioButton.Name = "ActionOnRadioButton";
-            this.ActionOnRadioButton.Size = new System.Drawing.Size(93, 21);
-            this.ActionOnRadioButton.TabIndex = 0;
-            this.ActionOnRadioButton.Text = "Включить";
-            this.ActionOnRadioButton.UseVisualStyleBackColor = true;
-            this.ActionOnRadioButton.CheckedChanged += new System.EventHandler(this.ActionOnRadioButton_CheckedChanged);
+            this.TableLayoutPanel.ColumnCount = 3;
+            this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.42936F));
+            this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.02493F));
+            this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.2687F));
+            this.TableLayoutPanel.Controls.Add(this.ConditionOperatorComboBox, 1, 0);
+            this.TableLayoutPanel.Controls.Add(this.ConditionFieldComboBox, 0, 0);
+            this.TableLayoutPanel.Controls.Add(this.ConditionValueTextBox, 2, 0);
+            this.TableLayoutPanel.Location = new System.Drawing.Point(11, 217);
+            this.TableLayoutPanel.Name = "TableLayoutPanel";
+            this.TableLayoutPanel.RowCount = 1;
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.TableLayoutPanel.Size = new System.Drawing.Size(361, 32);
+            this.TableLayoutPanel.TabIndex = 13;
             // 
-            // ActionOffRadioButton
+            // ConditionGroupBox
             // 
-            this.ActionOffRadioButton.AutoSize = true;
-            this.ActionOffRadioButton.Checked = true;
-            this.ActionOffRadioButton.Location = new System.Drawing.Point(7, 21);
-            this.ActionOffRadioButton.Name = "ActionOffRadioButton";
-            this.ActionOffRadioButton.Size = new System.Drawing.Size(103, 21);
-            this.ActionOffRadioButton.TabIndex = 1;
-            this.ActionOffRadioButton.TabStop = true;
-            this.ActionOffRadioButton.Text = "Выключить";
-            this.ActionOffRadioButton.UseVisualStyleBackColor = true;
-            this.ActionOffRadioButton.CheckedChanged += new System.EventHandler(this.ActionOffRadioButton_CheckedChanged);
+            this.ConditionGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ConditionGroupBox.Controls.Add(this.AddCondition);
+            this.ConditionGroupBox.Controls.Add(this.DeleteCondition);
+            this.ConditionGroupBox.Location = new System.Drawing.Point(11, 255);
+            this.ConditionGroupBox.Name = "ConditionGroupBox";
+            this.ConditionGroupBox.Size = new System.Drawing.Size(363, 52);
+            this.ConditionGroupBox.TabIndex = 7;
+            this.ConditionGroupBox.TabStop = false;
+            this.ConditionGroupBox.Text = "Добавление/удаление условий";
+            // 
+            // DeleteCondition
+            // 
+            this.DeleteCondition.Location = new System.Drawing.Point(105, 21);
+            this.DeleteCondition.Name = "DeleteCondition";
+            this.DeleteCondition.Size = new System.Drawing.Size(92, 22);
+            this.DeleteCondition.TabIndex = 14;
+            this.DeleteCondition.Text = "-";
+            this.DeleteCondition.UseVisualStyleBackColor = true;
+            this.DeleteCondition.Click += new System.EventHandler(this.DeleteCondition_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Условия";
             // 
             // CreateRuleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 492);
+            this.ClientSize = new System.Drawing.Size(381, 355);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.TableLayoutPanel);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.CreateButton);
             this.Controls.Add(this.groupBox4);
@@ -216,19 +258,20 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "CreateRuleForm";
             this.Text = "Добавление авто-правила";
-            this.ConditionGroupBox.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ConditionValueTextBox)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
             this.groupBox5.ResumeLayout(false);
+            this.TableLayoutPanel.ResumeLayout(false);
+            this.ConditionGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.GroupBox ConditionGroupBox;
         private System.Windows.Forms.NumericUpDown ConditionValueTextBox;
         private System.Windows.Forms.ComboBox ConditionOperatorComboBox;
         private System.Windows.Forms.ComboBox ConditionFieldComboBox;
@@ -242,5 +285,9 @@
         private System.Windows.Forms.Button AddCondition;
         private System.Windows.Forms.RadioButton ActionOffRadioButton;
         private System.Windows.Forms.RadioButton ActionOnRadioButton;
+        private System.Windows.Forms.TableLayoutPanel TableLayoutPanel;
+        private System.Windows.Forms.GroupBox ConditionGroupBox;
+        private System.Windows.Forms.Button DeleteCondition;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FacebookApi/FacebookApiView/CreateRuleForm.cs
+++ FacebookApi/FacebookApiView/CreateRuleForm.cs
@@ -21,7 +21,9 @@ namespace FacebookApiView
         private string _conditionOperator;
         private string _conditionField;
         private string _action;
-        public CreateRuleForm(string acc, RequestExecutor re)
+        
+
+        public CreateRuleForm (string acc, RequestExecutor re)
         {
             InitializeComponent();
             _acc = acc;
@@ -40,15 +42,40 @@ namespace FacebookApiView
 
         private void AddCondition_Click(object sender, EventArgs e)
         {
+            int i = 1;
             
-            
-            //ConditionGroupBox.Size = new Size(363, HeightGroupBox=HeightGroupBox+20);
-            //var textBox = new TextBox();
-            //textBox.Top = ConditionFieldComboBox.Top-100;
-            //textBox.Left = ConditionFieldComboBox.Left;
-            //Controls.Add(textBox);
+            ComboBox field = new ComboBox();
+            ComboBox op = new ComboBox();
+            NumericUpDown value = new NumericUpDown();
+
+            field.Name = "ConditionFieldComboBox" + i;
+            op.Name = "ConditionOperatorComboBox" + i;
+            value.Name = "ConditionValueComboBox" + i;
+
+            string[] fields = { "Цена за результат", "Результаты", "Расходы" };
+            string[] ops = { "Больше, чем", "Меньше, чем", "Равно" };
+
+            field.Items.AddRange(fields);
+            op.Items.AddRange(ops);
+
+            field.Size = new Size(158, 24); // размер
+            field.Text = "Цена за результат"; //текст
+            op.Size = new Size(106, 24); // размер
+            op.Text = "Больше, чем"; //текст
+            value.Size = new Size(75, 22); // размер
+
+            TableLayoutPanel.RowCount = ++TableLayoutPanel.RowCount;
+            TableLayoutPanel.Size = new Size(361, TableLayoutPanel.Size.Height + 30);
+            TableLayoutPanel.RowStyles.Add(new RowStyle());
+            TableLayoutPanel.Controls.Add(field, 0, TableLayoutPanel.RowCount -1 );
+            TableLayoutPanel.Controls.Add(op, 1, TableLayoutPanel.RowCount -1);
+            TableLayoutPanel.Controls.Add(value, 2, TableLayoutPanel.RowCount -1 );
+
+            this.Height += 30;
+            i++;
         }
 
+      
         private void CloseButton_Click(object sender, EventArgs e)
         {
             Close();
@@ -110,6 +137,25 @@ namespace FacebookApiView
         private void ActionOnRadioButton_CheckedChanged(object sender, EventArgs e)
         {
             _action = "UNPAUSE";
+        }
+
+        private void DeleteCondition_Click(object sender, EventArgs e)
+        {
+            int row = TableLayoutPanel.RowCount - 1;
+            if (row > 0)
+            {
+                for (int i = 0; i < TableLayoutPanel.ColumnCount; i++)
+                {
+                    Control c = TableLayoutPanel.GetControlFromPosition(i, row);
+                    TableLayoutPanel.Controls.Remove(c);
+                    c.Dispose();
+                }
+
+                TableLayoutPanel.RowStyles.RemoveAt(row);
+                TableLayoutPanel.RowCount--;
+                TableLayoutPanel.Size = new Size(361, TableLayoutPanel.Size.Height - 30);
+                this.Height -= 30;
+            }            
         }
     }
 }
